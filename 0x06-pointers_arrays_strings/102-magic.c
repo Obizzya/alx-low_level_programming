bc45cd9c4e64087a7a0a785f84babe016de0e09a
obizzya <ocenarnold@gmail.com>
2022-07-15 14:18:39 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 1024;
+	a[2] = 98;
 	p = &n;
 	/*
 	 * writ your line of code here...
@@ -16,7 +16,7 @@ int main(void)
 	 * - You can only write one statement
 	 * - You are not allowed to code anything else than the line of expected
 	 */
-	*(p + 3) = 98;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
