9a4311bad51cec5627ad978cbf74d2a95d564f16
obizzya <ocenarnold@gmail.com>
2022-07-26 05:53:19 -0700

    3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	if (result == NULL)
 		return (NULL);
 
-	for (i = 0; < len1; i++)
+	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
