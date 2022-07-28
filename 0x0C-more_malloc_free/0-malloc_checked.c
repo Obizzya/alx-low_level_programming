b625e2f8f1b026f1fd7fcf8ca5a0f1ae7f9177b4
obizzya <ocenarnold@gmail.com>
2022-07-28 08:09:59 -0700

    crc 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *malloc_checked - allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
  * @b: the memory size
  * Return: a pointer to the allocated memory
  */
