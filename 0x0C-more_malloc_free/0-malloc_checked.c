87ba9031429ee0583d644d6d2245296e12cd03be
obizzya <ocenarnold@gmail.com>
2022-07-28 08:01:53 -0700

    crc 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 /**
  * *malloc_checked - allocates memory using malloc
  * @b: the memory size
- * Return: a pointer to the located memory
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
